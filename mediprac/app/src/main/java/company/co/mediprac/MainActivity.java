dfff5e3f26e85a682ff7709ec5e42c81243970d1
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-05-28 21:25:47 +0900

    main&search setting

--- mediprac/app/src/main/java/company/co/mediprac/MainActivity.java
+++ mediprac/app/src/main/java/company/co/mediprac/MainActivity.java
@@ -1,8 +1,11 @@
 package company.co.mediprac;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+//import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_search = findViewById(R.id.button);
+        button_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_search = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent_search);
+            }
+        });
+
+        Button b = (Button)findViewById(R.id.button4);
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(
+//                        getApplicationContext(), // 현재 화면의 제어권자
+//                        ShareActivity.class); // 다음 넘어갈 클래스 지정
+//                startActivity(intent); // 다음 화면으로 넘어간다
+//            }
+//        });
     }
 }
