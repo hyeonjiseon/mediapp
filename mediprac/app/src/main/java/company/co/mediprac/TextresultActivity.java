2155f4beabbcb54817a9641bac320318beb0ee92
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-06-12 14:52:27 +0900

    don't need new class Asnyc
    i think

--- mediprac/app/src/main/java/company/co/mediprac/TextresultActivity.java
+++ mediprac/app/src/main/java/company/co/mediprac/TextresultActivity.java
@@ -1,5 +1,6 @@
 package company.co.mediprac;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,7 +11,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class TextresultActivity extends AppCompatActivity {
@@ -22,6 +27,12 @@ public class TextresultActivity extends AppCompatActivity {
     //ArrayList<Recent> items = null;
     ArrayList<Recent> items = new ArrayList<>();
 
+    public String requestUrl;
+    ArrayList<Recent> list = null;
+    Recent bus = null;
+    XmlPullParser xpp;
+    public String key="BZAkHyL1OvsaKk4INUgYd1ra39ts5cl%2BDojvvOH%2BQkW3FCIifva%2FTa5ZTKvrIt03W97NKmFMZH4Oq%2B6jIwy5bA%3D%3D";
+    String data;
 
 
     private RecyclerView.Adapter mAdapter;
@@ -114,69 +125,69 @@ public class TextresultActivity extends AppCompatActivity {
 
         });
 
-//        public class MyAsyncTask extends AsyncTask<String, Void, String> {
-//
-//            @Override
-//            protected String doInBackground(String... strings){
-//                requestUrl = "http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService/getMdcinPrductList?&ServiceKey="+key;
-//                try{
-//                    boolean b_ITEM_NAME = false;
-//                    boolean b_ENTP_NAME = false;
-//
-//                    URL url = new URL(requestUrl);
-//                    InputStream is = url.openStream();
-//                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-//                    XmlPullParser parser = factory.newPullParser();
-//                    parser.setInput(new InputStreamReader(is, "UTF-8"));
-//
-//                    String tag;
-//                    int evenType = parser.getEventType();
-//
-//                    while(evenType != XmlPullParser.END_DOCUMENT){
-//                        switch(evenType){
-//                            case XmlPullParser.START_DOCUMENT:
-//                                list = new ArrayList<Recent>();
-//                                break;
-//                            case XmlPullParser.END_DOCUMENT:
-//                                break;
-//                            case XmlPullParser.END_TAG:
-//                                if(parser.getName().equals("item") && bus != null){
-//                                    list.add(bus);
-//                                }
-//                                break;
-//                            case XmlPullParser.START_TAG:
-//                                if(parser.getName().equals("item")){
-//                                    bus = new Recent();
-//                                }
-//                                if(parser.getName().equals("ITEM_NAME")) b_ITEM_NAME = true;
-//                                if(parser.getName().equals("ENTP_NAME")) b_ENTP_NAME = true;
-//                                break;
-//                            case XmlPullParser.TEXT:
-//                                if(b_ITEM_NAME){
-//                                    bus.setITEM_NAME(parser.getText());
-//                                    b_ITEM_NAME = false;
-//                                } else if(b_ENTP_NAME){
-//                                    bus.setENTP_NAME(parser.getText());
-//                                    b_ENTP_NAME = false;
-//                                }
-//                                break;
-//                        }
-//                        evenType = parser.next();
-//                    }
-//                } catch(Exception e){
-//                    e.printStackTrace();
-//                }
-//                return null;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(String s){
-//                super.onPostExecute(s);
-//                //어답터 연결
-//                RecyclerviewAdapter adapter = new RecyclerviewAdapter(getApplicationContext(), items);
-//                recyclerView.setAdapter(adapter);
-//            }
-//        }
+        class MyAsyncTask extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... strings){
+                requestUrl = "http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService/getMdcinPrductList?&ServiceKey="+key;
+                try{
+                    boolean b_ITEM_NAME = false;
+                    boolean b_ENTP_NAME = false;
+
+                    URL url = new URL(requestUrl);
+                    InputStream is = url.openStream();
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    XmlPullParser parser = factory.newPullParser();
+                    parser.setInput(new InputStreamReader(is, "UTF-8"));
+
+                    String tag;
+                    int evenType = parser.getEventType();
+
+                    while(evenType != XmlPullParser.END_DOCUMENT){
+                        switch(evenType){
+                            case XmlPullParser.START_DOCUMENT:
+                                list = new ArrayList<Recent>();
+                                break;
+                            case XmlPullParser.END_DOCUMENT:
+                                break;
+                            case XmlPullParser.END_TAG:
+                                if(parser.getName().equals("item") && bus != null){
+                                    list.add(bus);
+                                }
+                                break;
+                            case XmlPullParser.START_TAG:
+                                if(parser.getName().equals("item")){
+                                    bus = new Recent();
+                                }
+                                if(parser.getName().equals("ITEM_NAME")) b_ITEM_NAME = true;
+                                if(parser.getName().equals("ENTP_NAME")) b_ENTP_NAME = true;
+                                break;
+                            case XmlPullParser.TEXT:
+                                if(b_ITEM_NAME){
+                                    bus.setITEM_NAME(parser.getText());
+                                    b_ITEM_NAME = false;
+                                } else if(b_ENTP_NAME){
+                                    bus.setENTP_NAME(parser.getText());
+                                    b_ENTP_NAME = false;
+                                }
+                                break;
+                        }
+                        evenType = parser.next();
+                    }
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+                //어답터 연결
+                RecyclerviewAdapter adapter = new RecyclerviewAdapter(getApplicationContext(), items);
+                recyclerView.setAdapter(adapter);
+            }
+        }
 
 //        void filter (String text) {
 //            List<Recent> temp = new ArrayList();
@@ -334,4 +345,6 @@ public class TextresultActivity extends AppCompatActivity {
 //        return buffer.toString();//StringBuffer 문자열 객체 반환
 //
 //    }//getXmlData method....
+}
+
 }
